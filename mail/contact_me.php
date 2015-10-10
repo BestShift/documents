69b184d2a337723ab245b036677083513653c34f
Daniel Melichar <danielmelichar0905@gmail.com>
2015-10-10 23:34:34 +0200

    Mdae minor changes to text and images on website

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,11 @@ $message = $_POST['message'];
 $to = 'daniel@melichar.me'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+<<<<<<< HEAD
 $headers = "From: noreply@bestshift.at\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+=======
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+>>>>>>> fdb5502a353742327ae1761555269408dbe0f904
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
